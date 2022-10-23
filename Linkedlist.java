7a4bb7127ef3db6c1334e68ec4a7c8a6892c9d7d
karthik <karthikpatlolla22@gmail.com>
2022-10-23 23:59:13 +0530

    ll1

---
+++ Linkedlist.java
@@ -0,0 +1,56 @@
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        next = null;
+
+    }}
+
+    
+
+class Linkedlist{
+    static Node head = null;
+    static Node tail = null;
+    public static Node insert(int d)
+    {
+        Node n=new Node(d);
+        if(head==null)
+        {
+            head=n;
+            tail=n;
+        }
+        else
+        {
+            tail.next=n;
+            tail=n;
+        }
+        return head;
+        
+    } 
+    public static void print()
+    {
+         Node t=head;
+        while(t!=null)
+        {
+            System.out.println(t.data);
+            t=t.next;
+        }
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            int d=sc.nextInt();
+            head=insert(d);
+        }
+        print();
+        
+    }
+
+    
+}
---
+++ test.js
@@ -0,0 +1,10 @@
+var a = [1,2,3,4]
+class Db
+{
+    show = ()=>
+    {
+        return "hi"
+    }
+}
+var obj = new Db()
+console.log(obj.show())
